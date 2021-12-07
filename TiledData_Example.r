ad8f69a82ab9ce9a8d018ea51cff8ac2f57e75ea
Jiayong Li <jli@curii.com>
2021-12-07 17:04:08 +0000

    Fix indentation Arvados-DCO-1.1-Signed-off-by: Jiayong Li <jli@curii.com> refs #18468

--- TiledData_Example.r
+++ TiledData_Example.r
@@ -138,25 +138,24 @@ print(head(tiledata,20))
 
 # Helper function to look up tile variants in the annotation file
 print_annotation <- function(tiletag, varvals) {
-   pattern = paste("", tiletag, as.character(varvals), "", sep=",")
-   command = paste("grep", pattern, annotation_file)
-   returncode <- system(command)
-   # return code is 1 if and only if there is no matching result, i.e., if the tile variant is the same as ref
-   return(returncode)
+  pattern = paste("", tiletag, as.character(varvals), "", sep=",")
+  command = paste("grep", pattern, annotation_file)
+  returncode <- system(command)
+  # return code is 1 if and only if there is no matching result, i.e., if the tile variant is the same as ref
+  return(returncode)
 }
 
 print("Finding annotations to top 20 tile variants")
-
 for (i in 1:20) {
-   tiletag = tiledata$tiletag[i]
-   varvals = as.numeric(tiledata$varvals[i])-1 # -1 offset required for filtered data
-   rc = print_annotation(tiletag, varvals)
-   if (varvals != 1 & rc == 1) { # if that tile variant is ref, look up the most common tile variant in that position instead
-     for (i in 1:varvals-1) {
-       rc = print_annotation(tiletag, i)
-       if (rc == 0) {
+  tiletag = tiledata$tiletag[i]
+  varvals = as.numeric(tiledata$varvals[i])-1 # -1 offset required for filtered data
+  rc = print_annotation(tiletag, varvals)
+  if (varvals != 1 & rc == 1) { # if that tile variant is ref, look up the most common tile variant in that position instead
+    for (i in 1:varvals-1) {
+      rc = print_annotation(tiletag, i)
+      if (rc == 0) {
          break
-       }
-     }
-   }
+      }
+    }
+  }
 }
